db6f07332224879bdaf37d904ae1b00af80f4513
MSWA89 <sat.almalazi@gmail.com>
2022-10-19 04:58:56 +0200

    Final

---
+++ spaceship/README.md
@@ -0,0 +1,12 @@
+
+****************************************************Discprition****************************************************
+Space ship project, where there is a space ship located on the axis x and y, at coordination [0,0] lookling up towards (y+). the aim of this program is to take char string as an input, and move the space ship accordingly the string has 'A' for advance, 'L' for turning left and 'R' for turning right. at the end, the coords and direction of the ship should be handed in as a formatted string.
+
+***Steps***
+1. Create main for testing and string command input.
+2. Create the my_spaceship function
+    a. create and malloc the varialbles needed (coords, end string, index).
+    b. loop inside the input to determin the which function to call (advance function , or advance function) depending on the command string.
+3. Create the advance function
+4. Create the turn function
+5. test using "RRAALLAA" a. is [0,0] U, "RAALLARRALA" a. is [2,1] U, "RAAALARRAAAALLLAARALALALALALLLAAA" a. is [4,-2] R. 
--- spaceship/spaceship.c
+++ spaceship/spaceship.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-/* ****************************************************Discprition****************************************************
-Space ship project, where there is a space ship located on the axis x and y, att coordination [0,0] lookling up. the aim of this program is to take char string as an input, and move the space ship accordingly the string has 'A' for advance, 'L' for turning left and 'R' for turning right. at the end, the coords and direction of the ship should be handed in a formatted string.
 
-***Steps***
-1. Create main for testing and string command input.
-2. Create the my_spaceship function
-    a. create and malloc the varialbles needed (coords, end string, index).
-    b. loop inside the input to determin the which function to call (advance function , or advance function) depending on the command string.
-3. Create the advance function
-4. Create the turn function
-5. test using "RRAALLAA" a. is [0,0] U, "RAALLARRALA" a. is [2,1] U, "RAAALARRAAAALLLAARALALALALALLLAAA" a. is [4,-2] R. 
-*/
 int SetLoc;
 // step 4
 char turn(char c)
